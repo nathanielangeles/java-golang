8240bd803f4c3c3dbbef1f1a7941ab33ec7aa270
Nathaniel Angeles <166530667+nathanielangeles@users.noreply.github.com>
2025-03-24 09:45:11 +0800

    Create linearSearch.go

---
+++ DSA/linearSearch.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variables to store user input and a pointer.
+	var input int
+
+	// Declare a random array.
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// Ask the user which number to search for.
+	fmt.Print("[*] Enter a number to search for (1-9): ")
+	fmt.Scan(&input)
+
+	// Start searching.
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] == input {
+			fmt.Printf("Value: %v is found at index %v", input, i)
+			break
+		}
+	}
+}
